8682dd9e3cab8012ea62e1723998a6aa4d44b0de
aleks1212v <vitomskov@mail.ru>
2020-06-12 22:46:43 +0500

    HomeWork9 - Реализация экранов

--- app/src/main/java/com/example/aleks_vitomskov_shop/BasketPresenter.kt
+++ app/src/main/java/com/example/aleks_vitomskov_shop/BasketPresenter.kt
@@ -3,9 +3,9 @@ package com.example.aleks_vitomskov_shop
 import moxy.MvpPresenter
 
 class BasketPresenter : MvpPresenter<BasketView>() {
-    private val iphoneCase = Product(price = 170.0, salePercent = 20, productName = "iphoneCase")
-    private val samsungCase = Product(price = 180.0, salePercent = 30, productName = "samsungCase")
-    private val asusCase = Product(price = 198.0, salePercent = 60, productName = "asusCase")
+    private val iphoneCase = Product(price = 155.0, salePercent = 20, productName = "iphoneCase")
+    private val samsungCase = Product(price = 123.0, salePercent = 30, productName = "samsungCase")
+    private val asusCase = Product(price = 138.0, salePercent = 10, productName = "asusCase")
     private val basket: List<Product> = listOf(iphoneCase, samsungCase, asusCase)
 
     private val model = CreateOrderModel()
