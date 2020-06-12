eb05749e56ff130ba5ff272cbde6f5207e372cb0
aleks1212v <vitomskov@mail.ru>
2020-06-12 21:36:41 +0500

    HomeWork8 - Валидация номера телефона

--- app/src/main/java/com/example/aleks_vitomskov_shop/BasketView.kt
+++ app/src/main/java/com/example/aleks_vitomskov_shop/BasketView.kt
@@ -1,5 +1,24 @@
 package com.example.aleks_vitomskov_shop
 
-interface BasketView {
+import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface BasketView : MvpView {
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
     fun print(prices:String)
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun showErrorForSecondName(visible: Boolean)
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun showErrorForFirstName(visible: Boolean)
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun showErrorForMiddleName(visible: Boolean)
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun showErrorForPhone(visible: Boolean)
 }
